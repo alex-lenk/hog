92734d80304aa5e1e34819cef3099b2ec7590610
alex-iank <artlenk.ru@gmail.com>
2018-02-15 16:39:35 +0300

    Initial commit

---
+++ src/js/lib.js
@@ -0,0 +1,43 @@
+/*
+ *
+ * Content of libraries
+ * 1.   smoothscroll.js
+ * 2.   svgxuse.min.js
+ * 3.   jquery.fullPage.js
+ * 4.   swiper.min.js
+ * 5.   jquery.formstyler.min.js
+ * 6.   isotope.pkgd.min.js
+ * 7.   jquery.fancybox.min.js
+ * 8.   bootstrap.min.js
+ *
+ */
+
+/* 1. smoothscroll.js */
+//= lib/smoothscroll.js
+
+
+/* 2. svgxuse.min.js */
+//= lib/svgxuse.min.js
+
+
+/* 3. jquery.fullPage.js */
+// = ../../bower_components/fullPage.js/dist/jquery.fullpage.min.js
+
+
+/* 4. swiper.min.js */
+// = ../../bower_components/swiper/dist/js/swiper.min.js
+
+
+/* 5. jquery.formstyler.min.js */
+// = lib/jquery.formstyler.min.js
+
+
+/* 6. isotope.pkgd.min.js */
+// = ../../bower_components/isotope-layout/dist/isotope.pkgd.min.js
+
+
+/* 7. jquery.fancybox.min.js */
+//= ../../bower_components/fancybox/dist/jquery.fancybox.min.js
+
+/* 8. bootstrap.min.js */
+//= lib/bootstrap/bootstrap.min.js
